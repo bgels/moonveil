83f244dc7b49c95f7e4931f25dc2a32c3ed84d91
yu <ylu60@stuy.edu>
2026-02-23 00:31:36 -0500

    some other stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,15 @@
+import { TbEggCrackedFilled } from "react-icons/tb";
 import "./globals.css";
+import localFont from "next/font/local"
+
+const fontTegaki = localFont({
+  src:"./fonts/tegaki.woff2",
+  variable: "--font-tegaki"
+});
+const fontHandjet = localFont({
+  src:"./fonts/handjet.woff2",
+  variable: "--font-handjet-local"
+})
 
 export default function RootLayout({
   children,
@@ -6,11 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${fontTegaki.variable} ${fontHandjet.variable}`}>
       <body>
         {children}
       </body>
     </html>
   );
 }
-
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,55 @@
 "use client"
 import Image from "next/image";
 import { useUserPreferences } from "./store"
-import { PiMoonStarsFill } from "react-icons/pi";
-
+import { PiMoonStarsFill, PiGithubLogoFill, PiInstagramLogoFill, PiSteamLogoFill} from "react-icons/pi";
 
 export default function Home() {
   const darkMode = useUserPreferences(state => state.darkMode);
   const setDarkMode = useUserPreferences(state => state.setDarkMode);
 
   return (
-    <main className={`relative h-screen w-full ${darkMode ? "bg-gray-900" : "bg-blue-200"}`}>
-        
-          <PiMoonStarsFill onClick={() => setDarkMode()} 
-          className={`relative z-10 p-0 w-f-button h-f-button transition-all hover:scale-105 ${darkMode ? "text-blue-200" : "text-black"}`}/>
-
+    <main className={`relative h-screen w-full ${darkMode ? "bg-gray-900" : "bg-blue-300"}`}>
+      
+      {/* Background Layer */}
+      <div className="absolute opacity-10 inset-0 z-0 pointer-events-none">
         <Image 
           src="/homepage/bg.png" 
           fill
           alt="Background Image"
-          className="object-cover z-0" 
+          className="object-cover"
+          style={{ filter: "grayscale(100%) blur(7px) drop-shadow(8px 8px 10px lightblue)" }}
+          draggable="false"
           priority 
         />
+      </div>
+      
+      {/* Content Layer */}
+      <div className={`relative z-10 flex flex-row items-center justify-center h-full gap-8 ${darkMode ? "text-blue-200" : "text-black"}`}>
+        <p className={'[writing-mode:vertical-rl] select-none font-tegaki text-s-title tracking-tighter text-nowrap -mr-9 -ml-9'}>卢于</p>
+
+        <PiMoonStarsFill 
+          onClick={() => setDarkMode()} 
+          className={`relative w-s-button h-s-button transition-all hover:scale-105 ${darkMode ? "text-blue-200" : "text-black"}`}
+        />
+
+        <section className={`flex flex-col items-start gap-4 ${darkMode ? "text-blue-200" : "text-black"}`}>
+          <p className="text-2xl text-nowrap font-handjet">Yu Lu, Stuy 26'</p>
+          <hr className="w-full border-t"></hr>
+
+          <div className="flex flex-row items-center justify-items-center gap-4">
+            <a href="https://github.com/bgels" target="_blank" rel="noopener noreferrer">
+              <PiGithubLogoFill className={`w-s-smallButton h-s-smallButton hover:scale-105`}/>
+            </a>
+            <a href="https://www.instagram.com/youlucool/" target="_blank" rel="noopener noreferrer">
+              <PiInstagramLogoFill className={`w-s-smallButton h-s-smallButton hover:scale-105`}/>
+            </a>
+            <a href="https://steamcommunity.com/id/crusty_pizza/" target="_blank" rel="noopener noreferrer">
+              <PiSteamLogoFill className={`w-s-smallButton h-s-smallButton hover:scale-105`} />
+            </a>
+          </div>
+        </section>
+      </div>
+      
     </main>
   )
 }
